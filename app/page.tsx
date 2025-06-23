f1ca41664342cfa9f78186fa395469b3006d1b93
HARISH130505 <harish13.05.05@gmail.com>
2025-06-23 21:32:40 +0530

    new commit from harish

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Image from 'next/image'
+import next from 'next'
 const page = () => {
   return (
-    <div>
+    <div className='py-5'>
+      <div className='flex justify around items-center'>
+      <div>
+        <h1 className="text-orange-500 text-2xl">CODEZILLA|| MOZILLA COMMUNITY|| OPEN SOURCE|| SRM IST RMP</h1>
+      </div>
+      <Image
+        src="/photo.jpg"
+        alt="img"
+        height={500}
+        width={500}
+      />
+      </div>
     </div>
   )
 }
